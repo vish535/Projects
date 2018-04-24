4c78869403264012788d6e908f331180e6b77458
vishal <vishal535@outlook.com>
2018-04-24 20:31:20 +0530

    added name compare logic

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -12,7 +12,22 @@ namespace Hello
         {
             Console.WriteLine("Welcome vishal.");
             Console.WriteLine("Bhandekar.");
+
+            string result=Program.NameCompare("vishal", "vish");
+            Console.WriteLine(result);
             Console.Read();
+        }
+
+        public static string NameCompare(string FirstName,string knownAs)
+        {
+            if(FirstName==knownAs)
+            {
+                return "";
+            }
+            else
+            {
+                return knownAs;
+            }           
         }
     }
 }
